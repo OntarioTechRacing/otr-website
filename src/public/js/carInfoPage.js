ae9467992af4003c3a68d6f02a340cc8ccaf997a
odiat <troy.odia@ontariotechu.net>
2024-09-25 17:06:30 -0400

    added scroll animation for name and description of specifications

--- src/public/js/carInfoPage.js
+++ src/public/js/carInfoPage.js
@@ -15,12 +15,33 @@ const observer = new IntersectionObserver(
     threshold: [0, 0.1, 1],
   }
 );
+const observer1 = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("pop-in");
+        entry.target.classList.remove("not-in-view");
+      } else {
+        entry.target.classList.remove("pop-in");
+        entry.target.classList.add("not-in-view");
+      }
+    });
+  },
+  {
+    rootMargin: "0px",
+    threshold: [0, 0.1, 1],
+  }
+);
 
 const tags = document.querySelectorAll(
-  ".image,.table,.section-header, .background-information, .number-value, .comp"
+  ".image,.table,.section-header, .background-information, .number-value, .comp, .spec-description"
 );
+const specTags = document.querySelectorAll(".spec-name");
 tags.forEach((tag) => {
   observer.observe(tag);
+});
+specTags.forEach((tag) => {
+  observer1.observe(tag);
 });
 
 history.scrollRestoration = "manual";
