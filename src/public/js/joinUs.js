82c27eb5d00f2db001b3ef780c52cf809d225c63
odiat <troy.odia@ontariotechu.net>
2024-10-17 21:16:56 -0400

    added phone number verification

--- src/public/js/joinUs.js
+++ src/public/js/joinUs.js
@@ -10,6 +10,7 @@ const message = document.querySelector(".message");
 const firstNameErrorMessage = document.getElementById("firstname-error");
 const lastNameErrorMessage = document.getElementById("lastname-error");
 const emailErrorMessage = document.getElementById("email-error");
+const phoneErrorMessage = document.getElementById("phone-error");
 const verficationErrorMessage = document.getElementById(
   "email-verfication-error"
 );
@@ -20,13 +21,19 @@ const validateEmail = (email) => {
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   return regex.test(email);
 };
+const validatePhoneNumber = (phone) => {
+  const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
 
+  return regex.test(phone);
+};
 sendButton.addEventListener("click", (e) => {
   e.preventDefault();
   firstNameErrorMessage.style.display = "none";
   lastNameErrorMessage.style.display = "none";
   emailErrorMessage.style.display = "none";
   messageErrorMessage.style.display = "none";
+  phoneErrorMessage.style.display = "none";
+
   const isEmail = validateEmail(email.value);
   console.log(isEmail);
   if (
@@ -36,6 +43,13 @@ sendButton.addEventListener("click", (e) => {
     message.value &&
     isEmail
   ) {
+    if (phoneNumber.value) {
+      console.log(validatePhoneNumber(phoneNumber.value));
+      if (!validatePhoneNumber(phoneNumber.value)) {
+        phoneErrorMessage.style.display = "flex";
+        return;
+      }
+    }
     joinUsForm.style.display = "none";
     thankYouMessage.style.display = "flex";
     sendEmail(
@@ -60,6 +74,12 @@ sendButton.addEventListener("click", (e) => {
     }
     if (email.value && !isEmail) {
       verficationErrorMessage.style.display = "flex";
+    }
+    if (phoneNumber.value) {
+      if (!validatePhoneNumber(phoneNumber.value)) {
+        phoneErrorMessage.style.display = "flex";
+        return;
+      }
     }
   }
 });
