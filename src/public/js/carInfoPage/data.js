9698d799192af18f2fc4ece4088d95359d6cf14b
odiat <troy.odia@ontariotechu.net>
2024-11-15 20:25:14 -0500

    preliminary data array for car info pages

---
+++ src/public/js/carInfoPage/data.js
@@ -0,0 +1,112 @@
+const pageInfoData = [
+  {
+    title: {
+      titleName: "OTR F2024",
+      titleImage: "/images/carImages/2024/title image.jpg",
+      //get tag line
+      tagLine: "tag line",
+    },
+    background: {
+      //get background info
+      backgroundInfo: `
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece ofclassical Latin literature from 45 BC, making
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making
+        it over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going
+        through the cites of the word in classical literature, discovered the
+        undoubtable source. Lorem Ipsum comes fro10.32`,
+      backgroundImage: "/images/carImages/2024/background image.jpg",
+    },
+    //get team members
+    team: {
+      member1: ["member", "Dep1"],
+      member1: ["member", "Dep1"],
+      member1: ["member", "Dep1"],
+      member1: ["member", "Dep1"],
+    },
+    Competition: {
+      competitionName: "Formula SAE Electric",
+      competitionLocation: "Michigan Int'l Speedway in Brooklyn, Michigan",
+      competitionDate: "June 12-15 2024",
+      competitionImage: "/images/carImages/2024/comp image.png",
+      placement: "52nd",
+    },
+    //get specs
+    specifications: {
+      motor: {
+        name: "Stock Suzuki GSXR 600",
+        image: "images/spec-images/suzuki GSXR black.jpg",
+      },
+      motor: {
+        name: "Stock Suzuki GSXR 600",
+        image: "images/spec-images/suzuki GSXR black.jpg",
+      },
+      motor: {
+        name: "Stock Suzuki GSXR 600",
+        image: "images/spec-images/suzuki GSXR black.jpg",
+      },
+    },
+    slideshow: [
+      "/images/carImages/2024/slideshow 1.jpg",
+      "/images/carImages/2024/slideshow 2.jpg",
+      "/images/carImages/2024/slideshow 3.jpg",
+      "/images/carImages/2024/slideshow 4.jpg",
+      "/images/carImages/2024/slideshow 5.jpg",
+      "/images/carImages/2024/slideshow 6.jpg",
+      "/images/carImages/2024/slideshow 7.jpg",
+    ],
+  },
+  {
+    title: {
+      titleName: "OTR F2022",
+      titleImage: "/images/carImages/2022/title image.jpg",
+      tagLine: "'Zippy' LEGACY VEHICLE",
+    },
+    background: {
+      backgroundInfo: `
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece ofclassical Latin literature from 45 BC, making
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making
+        it over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going
+        through the cites of the word in classical literature, discovered the
+        undoubtable source. Lorem Ipsum comes fro10.32`,
+      backgroundImage: "images/carImages/2022/background inage.jpg",
+    },
+    team: {
+      member1: ["member", "Dep1"],
+      member1: ["member", "Dep1"],
+      member1: ["member", "Dep1"],
+      member1: ["member", "Dep1"],
+    },
+    Competition: {
+      competitionName: "Formula SAE Electric",
+      competitionLocation: "Michigan Int'l Speedway in Brooklyn, Michigan",
+      competitionDate: "June 15-18 2024",
+      competitionImage: "/images/carImages/2022/comp image.png",
+      placement: "31st",
+    },
+    specifications: {
+      motor: {
+        name: "Emrax 208",
+        image: "/images/spec-images/2022/ermax.jpeg",
+      },
+      power: "Peak 80kW",
+      torque: "Peak 140Nm",
+    },
+    slideshow: [
+      "/images/carImages/2022/slideshow 1.jpg",
+      "/images/carImages/2022/slideshow 2.jpg",
+      "/images/carImages/2022/slideshow 3.jpg",
+      "/images/carImages/2022/slideshow 4.jpg",
+      "/images/carImages/2022/slideshow 5.jpg",
+      "/images/carImages/2022/slideshow 6.jpg",
+      "/images/carImages/2022/slideshow 7.jpg",
+    ],
+  },
+];
