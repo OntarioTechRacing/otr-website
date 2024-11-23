17d5ff37c9f535015c31b8a74cad113e0cd316e2
odiat <troy.odia@ontariotechu.net>
2024-11-22 19:37:48 -0500

    created js generation of pages, added new images

---
+++ src/public/js/carInfoPage/carInfoRender.js
@@ -0,0 +1,155 @@
+import { pageInfoData } from "./data.js";
+
+const url = new URL(window.location.href);
+// const carYear = url.searchParams.get('year');
+const carYear = "OTR F2022";
+
+const CurrentPageData = pageInfoData.find(
+  (element) => element.title.titleName === carYear
+);
+{
+}
+let pageHTML =
+  CurrentPageData &&
+  `
+     <div class="title-section">
+      <div
+       class="title">${CurrentPageData.title.titleName}
+    </div>
+    <div class="title-image">
+        <img src=${CurrentPageData.title.titleImage} alt="" />
+    </div> 
+      <div class="car-quote"><p>${CurrentPageData.title.tagLine}</p></div>
+    </div>
+
+    <div class="background-section">
+      <div class="background">
+        <div class="background-heading section-header">BACKGROUND</div>
+        <p class="background-information">
+         ${CurrentPageData.background.backgroundInfo}
+        </p>
+      </div>
+      <div class="background-image">
+        <img class="image" src=${
+          CurrentPageData.background.backgroundImage
+        } alt="" />
+      </div>
+    </div>
+    <div class="team-section">
+      <div class="team-heading section-header">THE TEAM</div>
+      <div class="team-grid">
+        <div class="member-col table">
+          <div>Team Member</div>
+          ${CurrentPageData.team
+            .map((element) => {
+              return `
+                      <div>${element[0]}</div>
+
+            `;
+            })
+            .join("")}
+          
+        </div>
+        <div></div>
+        <div class="dept-col table">
+          <div>Department</div>
+          ${CurrentPageData.team
+            .map((element) => {
+              return `
+                      <div>${element[1]}</div>
+
+            `;
+            })
+            .join("")}
+        </div>
+      </div>
+    </div>
+    <div class="competition-section">
+      <div class="competition-heading section-header">COMPETITIONS</div>
+      <div class="competition-grid">
+        <div class="competition-info-row comp">
+          <div>${CurrentPageData.Competition.competitionName}</div>
+          <div>Placed ${CurrentPageData.Competition.placement}</div>
+        </div>
+        <div class="competition-image">
+          <img
+            class="image"
+            src=${CurrentPageData.Competition.competitionImage}
+            alt=""
+          />
+        </div>
+        <div class="competition-info comp">
+          <div>${CurrentPageData.Competition.competitionName}</div>
+          <div>&#8618; ${CurrentPageData.Competition.competitionLocation}</div>
+          <div>&#8618; ${CurrentPageData.Competition.competitionDate}</div>
+          <div>&#8618; ${CurrentPageData.Competition.placement}</div>
+        </div>
+        <div class="competition-info-row comp">
+          <div>${CurrentPageData.Competition.competitionLocation}</div>
+          <div>${CurrentPageData.Competition.competitionDate}</div>
+        </div>
+      
+      </div>
+    </div>
+    <div class="spec-section">
+      <div class="spec-heading section-header">SPECIFICATIONS</div>
+      <div class="spec-grid">
+      ${CurrentPageData.specifications
+        .map((element) => {
+          return `
+        <div class="specification">
+          <div class="spec-name">${element.spec}</div>
+          <div class="spec-description">${
+            element.image ? element.value : ""
+          }</div>
+
+         ${
+           element.image
+             ? `<div class="spec-image">
+            <img
+              class="image"
+              src=${element.image}
+              alt=""
+            />
+          </div>`
+             : `          
+             <div class="spec-number-value number-value">${element.value}</div>
+                `
+         }
+        </div>
+        `;
+        })
+        .join("")}
+      </div>
+    </div>
+    <div class="slideshow">
+      <div class="slideshow-heading section-header">SLIDESHOW</div>
+      <div class="slideshow-container">
+        <div class="slideshow-images">
+          <button class="left-arrow">
+            <img src="../images/icons/arrow-left.webp" alt="" />
+          </button>
+          ${CurrentPageData.slideshow
+            .map((image, index) => {
+              return `
+             <div>
+            <img
+              class="slideimage"
+              src=${image}
+              alt=""
+              data-id=${index}
+            />
+          </div>
+            `;
+            })
+            .join("")}
+          <button class="right-arrow">
+            <img src="../images/icons/arrow-right.webp" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+
+`;
+// console.log(pageHTML);
+document.querySelector(".page-content").innerHTML = pageHTML;
--- src/public/js/carInfoPage/data.js
+++ src/public/js/carInfoPage/data.js
@@ -1,8 +1,8 @@
-const pageInfoData = [
+export const pageInfoData = [
   {
     title: {
       titleName: "OTR F2024",
-      titleImage: "/images/carImages/2024/title image.jpg",
+      titleImage: "images/carImages/2024/title-image.jpg",
       //get tag line
       tagLine: "tag line",
     },
@@ -18,51 +18,54 @@ const pageInfoData = [
         Latin words, consectetur, from a Lorem Ipsum passage, and going
         through the cites of the word in classical literature, discovered the
         undoubtable source. Lorem Ipsum comes fro10.32`,
-      backgroundImage: "/images/carImages/2024/background image.jpg",
+      backgroundImage: "../../images/carImages/2024/background-image.jpg",
     },
     //get team members
-    team: {
-      member1: ["member", "Dep1"],
-      member1: ["member", "Dep1"],
-      member1: ["member", "Dep1"],
-      member1: ["member", "Dep1"],
-    },
+    team: [
+      ["member", "Dep1"],
+      ["member", "Dep1"],
+      ["member", "Dep1"],
+      ["member", "Dep1"],
+    ],
     Competition: {
       competitionName: "Formula SAE Electric",
       competitionLocation: "Michigan Int'l Speedway in Brooklyn, Michigan",
       competitionDate: "June 12-15 2024",
-      competitionImage: "/images/carImages/2024/comp image.png",
+      competitionImage: "../../images/carImages/2024/comp-image.png",
       placement: "52nd",
     },
     //get specs
-    specifications: {
-      motor: {
-        name: "Stock Suzuki GSXR 600",
-        image: "images/spec-images/suzuki GSXR black.jpg",
+    specifications: [
+      {
+        spec: "Motor",
+        value: "Stock Suzuki GSXR 600",
+        image: "../../images/spec-images/suzuki GSXR black.jpg",
       },
-      motor: {
-        name: "Stock Suzuki GSXR 600",
-        image: "images/spec-images/suzuki GSXR black.jpg",
+      {
+        spec: "Motor",
+        value: "Stock Suzuki GSXR 600",
+        image: "../../images/spec-images/suzuki GSXR black.jpg",
       },
-      motor: {
-        name: "Stock Suzuki GSXR 600",
-        image: "images/spec-images/suzuki GSXR black.jpg",
+      {
+        spec: "Motor",
+        value: "Stock Suzuki GSXR 600",
+        image: "../../images/spec-images/suzuki GSXR black.jpg",
       },
-    },
+    ],
     slideshow: [
-      "/images/carImages/2024/slideshow 1.jpg",
-      "/images/carImages/2024/slideshow 2.jpg",
-      "/images/carImages/2024/slideshow 3.jpg",
-      "/images/carImages/2024/slideshow 4.jpg",
-      "/images/carImages/2024/slideshow 5.jpg",
-      "/images/carImages/2024/slideshow 6.jpg",
-      "/images/carImages/2024/slideshow 7.jpg",
+      "../../images/carImages/2024/slideshow-1.jpg",
+      "../../images/carImages/2024/slideshow-2.jpg",
+      "../../images/carImages/2024/slideshow-3.jpg",
+      "../../images/carImages/2024/slideshow-4.jpg",
+      "../../images/carImages/2024/slideshow-5.jpg",
+      "../../images/carImages/2024/slideshow-6.jpg",
+      "../../images/carImages/2024/slideshow-7.jpg",
     ],
   },
   {
     title: {
       titleName: "OTR F2022",
-      titleImage: "/images/carImages/2022/title image.jpg",
+      titleImage: "../images/carImages/2022/titleimage.png",
       tagLine: "'Zippy' LEGACY VEHICLE",
     },
     background: {
@@ -76,37 +79,38 @@ const pageInfoData = [
         Latin words, consectetur, from a Lorem Ipsum passage, and going
         through the cites of the word in classical literature, discovered the
         undoubtable source. Lorem Ipsum comes fro10.32`,
-      backgroundImage: "images/carImages/2022/background inage.jpg",
+      backgroundImage: "../images/carImages/2022/background-image.jpg",
     },
-    team: {
-      member1: ["member", "Dep1"],
-      member1: ["member", "Dep1"],
-      member1: ["member", "Dep1"],
-      member1: ["member", "Dep1"],
-    },
+    team: [
+      ["member", "Dep1"],
+      ["member", "Dep1"],
+      ["member", "Dep1"],
+      ["member", "Dep1"],
+    ],
     Competition: {
       competitionName: "Formula SAE Electric",
       competitionLocation: "Michigan Int'l Speedway in Brooklyn, Michigan",
-      competitionDate: "June 15-18 2024",
-      competitionImage: "/images/carImages/2022/comp image.png",
+      competitionDate: "June 15-18, 2024",
+      competitionImage: "../images/carImages/2022/comp-image.jpg",
       placement: "31st",
     },
-    specifications: {
-      motor: {
-        name: "Emrax 208",
-        image: "/images/spec-images/2022/ermax.jpeg",
+    specifications: [
+      {
+        spec: "Motor",
+        value: "Emrax 208",
+        image: "../images/spec-images/2022/ermax1-nobg.png",
       },
-      power: "Peak 80kW",
-      torque: "Peak 140Nm",
-    },
+      { spec: "Power", value: "Peak 80kW" },
+      { spec: "Torque", value: "Peak 140Nm" },
+    ],
     slideshow: [
-      "/images/carImages/2022/slideshow 1.jpg",
-      "/images/carImages/2022/slideshow 2.jpg",
-      "/images/carImages/2022/slideshow 3.jpg",
-      "/images/carImages/2022/slideshow 4.jpg",
-      "/images/carImages/2022/slideshow 5.jpg",
-      "/images/carImages/2022/slideshow 6.jpg",
-      "/images/carImages/2022/slideshow 7.jpg",
+      "../images/carImages/2022/slideshow-1.jpg",
+      "../images/carImages/2022/slideshow-2.jpg",
+      "../images/carImages/2022/slideshow-3.jpg",
+      "../images/carImages/2022/slideshow-4.jpg",
+      "../images/carImages/2022/slideshow-5.jpg",
+      "../images/carImages/2022/slideshow-6.jpg",
+      "../images/carImages/2022/slideshow-7.jpg",
     ],
   },
 ];
