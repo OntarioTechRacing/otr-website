f5990d955a4221e5c77f4667550dc4a253905e94
troyodia <troy.odia@ontariotechu.net>
2025-01-03 18:29:11 -0500

    updated UI for footer positioning and to stop con

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -18,20 +18,3 @@ window.addEventListener("resize", () => {
     menuButton.classList.remove("open");
   }
 });
-
-    menuButton.classList.toggle("open");
-    if (!nav.classList.contains("nav-open")) {
-        nav.classList.add("nav-open");
-        body.style.marginTop = "318px";
-    } else {
-        nav.classList.remove("nav-open");
-        body.style.marginTop = "56px";
-    }
-});
-window.addEventListener("resize", () => {
-    if (window.innerWidth > 700) {
-        body.style.marginTop = "56px";
-        nav.classList.remove("nav-open");
-        menuButton.classList.remove("open");
-    }
-});
