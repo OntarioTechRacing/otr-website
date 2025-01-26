447de199cfa02e583ba1ec18b8498bc311458387
troyodia <troy.odia@ontariotechu.net>
2025-01-25 21:33:16 -0500

    updated join us and car information page UI

--- src/public/js/joinUs/email.js
+++ src/public/js/joinUs/email.js
@@ -1,6 +1,7 @@
-import { publicKey, serviceID, templateID } from "./apiKeys.js";
-emailjs.init(publicKey);
-
+// import { publicKey, serviceID, templateID } from "./apiKeys.js";
+// const publicKey = "I9qdyBOY1LRq-z7LE";
+const serviceID = "service_aa4r1no";
+const templateID = "template_ek8hq1d";
 export function sendEmail(firstName, lastName, email, message, phoneNumber) {
   const data = {
     firstname: firstName,
